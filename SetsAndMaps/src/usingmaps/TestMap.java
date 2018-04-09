74717d5bf2b74ccd6dfd57ed09b460219da271c2
cstuser <cstuser>
2018-04-09 17:16:22 -0400

    Fix merge error

--- SetsAndMaps/src/usingmaps/TestMap.java
+++ SetsAndMaps/src/usingmaps/TestMap.java
@@ -12,7 +12,7 @@ public class TestMap
         // Create a HashMap
         Map<String,Integer> hashMap = new HashMap();
         hashMap.put("Smith",30);
-        hashMap.put("Anderson",31);
+        hashMap.put("Anderson",31);      
         hashMap.put("Lewis",29);
         hashMap.put("Cook",29);
         
