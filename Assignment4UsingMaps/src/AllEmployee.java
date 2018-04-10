ffb5e9b38b430584c1f914af2b4d3ffcd7ce391f
Dylan Bobb <dylan.bobb@hotmail.com>
2018-04-10 12:13:40 -0400

    Added Description For AllEmployee class

---
+++ Assignment4UsingMaps/src/AllEmployee.java
@@ -0,0 +1,6 @@
+// This class creates employees (To be stored in a HashMap)
+// The key of this hashmap will be the employee number so that employees can be searched by their id number.(Key = idNum, Value = Employee)
+public class AllEmployee
+{
+    
+}
