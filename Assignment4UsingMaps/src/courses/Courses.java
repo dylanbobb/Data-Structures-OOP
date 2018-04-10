d95704f3c9fa33728f7a68e425695dc92e0b38ba
Dylan Bobb <dylan.bobb@hotmail.com>
2018-04-10 16:22:59 -0400

    Completed Basic Outline For Q3

---
+++ Assignment4UsingMaps/src/courses/Courses.java
@@ -0,0 +1,41 @@
+package courses;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Courses
+{
+    public static void main(String[] args)
+    {
+        ArrayList<String> mixedCourseList = new ArrayList();
+        mixedCourseList.add("1 CS100");
+        mixedCourseList.add("2 MATH210");
+        mixedCourseList.add("2 CS105");
+        mixedCourseList.add("1 CS200");
+        
+        HashMap<Integer,ArrayList<String>> students = new HashMap();
+
+        for(String x:mixedCourseList)
+        {
+            Scanner sc = new Scanner(x);
+            int studentNumber = Integer.parseInt(sc.next());
+            String courseName = sc.next();
+            
+            if(students.containsKey(studentNumber))
+            {
+                ArrayList<String> courseList = students.get(studentNumber);
+                courseList.add(courseName);
+                students.put(studentNumber, courseList);
+            }
+            else
+            {
+                ArrayList<String> courseList = new ArrayList();
+                courseList.add(courseName);
+                students.put(studentNumber, courseList);
+            }
+        }
+        
+        students.forEach((k,v) -> System.out.println(k + "\t" + v));
+    }
+}
