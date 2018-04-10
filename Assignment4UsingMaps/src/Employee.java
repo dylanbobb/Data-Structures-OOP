13ad9e43a5199e3358f7e54d79e38859648ebe38
Dylan Bobb <dylan.bobb@hotmail.com>
2018-04-10 12:15:23 -0400

    Added constructor for Employee class

--- Assignment4UsingMaps/src/Employee.java
+++ Assignment4UsingMaps/src/Employee.java
@@ -3,4 +3,11 @@ public class Employee
 {
     private int idNum;
     private String name;
+    
+    // 2 Argument Constructor
+    public Employee(int idNum, String name)
+    {
+        this.idNum = idNum;
+        this.name = name;
+    }
 }
