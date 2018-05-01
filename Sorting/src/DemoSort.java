c0d7f43f443829f75ae4031501190e1f6a25a332
Dylan Bobb <dylan.bobb@hotmail.com>
2018-05-01 12:02:12 -0400

    Generic Bubble, Insertion, and Select sorting

--- Sorting/src/DemoSort.java
+++ Sorting/src/DemoSort.java
@@ -1,12 +1,15 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.LinkedList;
+import java.util.List;
 
 public class DemoSort
 {
     public static void main(String[] args)
     {
-        int[] ar = {2,7,322,46,1,7,6,1,5,6,7,653,-3};
-        bubbleSort(ar);
+        double[] ar = {2,7,322,46,1,7,6,1,5,6,7,653,-3};
+        insertionSort(ar);
         System.out.println(Arrays.toString(ar));
     }
     
@@ -21,6 +24,34 @@ public class DemoSort
                 list[k+1] = list[k];
             }
             list[k+1] = currentElement;
+        }
+    }
+    
+//    public static void insertionSort(ArrayList<Integer> list)
+//    {
+//        for(int i = 1; i < list.size(); i++)
+//        {
+//            int currentElement = list.get(i);
+//            int k;
+//            for(k = i -1; k>= 0 && list.get(k) > currentElement; k--)
+//            {
+//                list.set(k+1,list.get(k));
+//            }
+//            list.set(k+1,currentElement);
+//        }
+//    }
+    
+    public static<E extends Comparable> void insertionSort(LinkedList<E> list)
+    {
+        for(int i = 1; i < list.size(); i++)
+        {
+            E currentElement = list.get(i);
+            int k;
+            for(k = i - 1; k>=0 && list.get(k).compareTo(currentElement) > 0; k--)
+            {
+                list.set(k+1,list.get(k));
+            }
+            list.set(k+1,currentElement);
         }
     }
     
@@ -47,16 +78,39 @@ public class DemoSort
         }
     }
     
-    public static void selectionSort(LinkedList<Double> list)
+//    public static void selectionSort(LinkedList<Integer> list)
+//    {
+//        for(int i = 0; i < list.size(); i++)
+//        {
+//            int currentMax = list.get(i);
+//            int currentMaxIndex = i;
+//            for(int j = i + 1; j < list.size(); j++)
+//            {
+//                if(currentMax < list.get(j))
+//                { 
+//                    currentMax = list.get(j);
+//                    currentMaxIndex = j;
+//                }
+//            }
+//            
+//            if(currentMaxIndex != i)
+//            {
+//                list.set(currentMaxIndex,list.get(i));
+//                list.set(i,currentMax);
+//            }
+//        }
+//    }
+    
+    public static<E extends Comparable> void selectionSort(LinkedList<E> list)
     {
         for(int i = 0; i < list.size(); i++)
         {
-            double currentMax = list.get(i);
+            E currentMax = list.get(i);
             int currentMaxIndex = i;
             for(int j = i + 1; j < list.size(); j++)
             {
-                if(currentMax < list.get(j))
-                { 
+                if(currentMax.compareTo(j) < 0)
+                {
                     currentMax = list.get(j);
                     currentMaxIndex = j;
                 }
@@ -83,6 +137,44 @@ public class DemoSort
                     int temp = list[i];
                     list[i] = list[i+1];
                     list[i+1] = temp;
+                    needNextPass = true;
+                }
+            }
+        }
+    }
+    
+//    public static void bubbleSort(LinkedList<Integer> list)
+//    {
+//       boolean needNextPass = true;
+//       for(int k = 1; k < list.size() && needNextPass; k++)
+//       {
+//           needNextPass = false;
+//           for(int i = 0; i < list.size() - k; i++)
+//           {
+//               if(list.get(i) > list.get(i+1))
+//               {
+//                   int temp = list.get(i);
+//                   list.set(i, list.get(i+1));
+//                   list.set(i+1,temp);
+//                   needNextPass = true;
+//               }
+//           }
+//       }
+//    }
+    
+    public static <E extends Comparable> void bubbleSort(LinkedList<E> list)
+    {
+        boolean needNextPass = true;
+        for(int k = 1; k < list.size() && needNextPass; k++)
+        {
+            needNextPass = false;
+            for(int i = 0; i < list.size() - k; i++)
+            {
+                if(list.get(i).compareTo(list.get(i+1)) > 0)
+                {
+                    E temp = list.get(i);
+                    list.set(i,list.get(i+1));
+                    list.set(i+1,temp);
                     needNextPass = true;
                 }
             }
