353116864ca738d8fff7fe937df5190a9e4fd0b3
cstuser <cstuser>
2018-05-02 11:52:01 -0400

    Merge Sort

--- Sorting/src/DemoSort.java
+++ Sorting/src/DemoSort.java
@@ -8,15 +8,15 @@ public class DemoSort
 {
     public static void main(String[] args)
     {
-        LinkedList<Book> list = new LinkedList();
-        list.add(new Book(5,"A"));
-        list.add(new Book(43,"Z"));
-        list.add(new Book(2,"B"));
-        list.add(new Book(40,"X"));
-        insertionSort(list);
-        //selectionSort(list);
-        //bubbleSort(list);
-        System.out.println(list);
+        ArrayList<Book> bl = new ArrayList();
+        bl.add(new Book(2,"a"));
+        bl.add(new Book(16,"b"));
+        bl.add(new Book(1,"c"));
+        bl.add(new Book(5,"d"));
+        bl.add(new Book(5423,"e"));
+        
+        mergeSort(bl);
+        System.out.println(bl);
     }
     
     public static void insertionSort(double[] list)
@@ -184,6 +184,107 @@ public class DemoSort
                     needNextPass = true;
                 }
             }
+        }
+    }
+    
+    public static void mergeSort(int[] list)
+    {
+        if(list.length > 1)
+        {
+            // Merge sort the first half.
+            int[] firstHalf = new int[list.length/2];
+            System.arraycopy(list,0,firstHalf,0,list.length/2);
+            mergeSort(firstHalf);
+            // Merge sort yhe second half;
+            int secondHalfLength = list.length - list.length/2;
+            int[] secondHalf = new int[secondHalfLength];
+            System.arraycopy(list,list.length/2,secondHalf,0,secondHalfLength);
+            mergeSort(secondHalf);
+            // Merge firstHalf with secondHalf into list.
+            merge(firstHalf,secondHalf,list);
+        }
+    }
+    
+    public static void merge(int[] list1,int[] list2,int[] temp)
+    {
+        int current1 = 0;
+        int current2 = 0;
+        int current3 = 0;
+        
+        while(current1 < list1.length && current2 < list2.length)
+        {
+            if(list1[current1] < list2[current2])
+            {
+                temp[current3++] = list1[current1++];
+            }
+            else
+            {
+                temp[current3++] = list2[current2++];
+            }
+        }
+        while(current1 < list1.length)
+        {
+            temp[current3++] = list1[current1++];
+        }
+        while(current2 < list2.length)
+        {
+            temp[current3++] = list2[current2++];
+        }
+    }
+    
+    public static <E extends Comparable> void mergeSort(List<E> list)
+    {
+        if(list.size() > 1)
+        {
+            List<E> firstHalf = new ArrayList();
+            for(int i = 0; i < list.size() / 2; i++)
+            {
+                firstHalf.add(list.get(i));
+            }
+            mergeSort(firstHalf);
+            int secondHalfLength = list.size() - list.size() / 2;
+            List<E> secondHalf = new ArrayList(secondHalfLength);
+            for(int i = list.size() / 2; i < secondHalfLength; i++)
+            {
+                secondHalf.add(list.get(i));
+            }
+            mergeSort(secondHalf);
+            merge(firstHalf,secondHalf,list);
+        }
+    }
+    
+    public static <E extends Comparable> void merge(List<E> list1,List<E> list2,List<E> temp)
+    {
+        int current1 = 0;
+        int current2 = 0;
+        int current3 = 0;
+        
+        while(current1 < list1.size() && current2 < list2.size())
+        {
+            if(list1.get(current1).compareTo(list2.get(current2)) < 0)
+            {
+                temp.set(current3,list1.get(current1));
+                current3++;
+                current1++;
+            }
+            else
+            {
+                temp.set(current3,list2.get(current2));
+                current3++;
+                current2++;
+            }
+        }
+        while(current1 < list1.size())
+        {
+            temp.set(current3,list1.get(current1));
+            current3++;
+            current1++;
+        }
+        while(current2 < list2.size())
+        {
+            temp.set(current3,list2.get(current2));
+            current3++;
+            current2++;
         }
     }
 }
